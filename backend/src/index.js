d94cc993ba3627299a0f5a8884abbdffa51c0b64
PedroKautzmann <pedrokautzmann1@gmail.com>
2020-01-17 19:32:28 -0300

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const http = require("http");
+const routes = require("./routes");
+const { setupWebSocket } = require("./websocket");
+const getMongoDB = require("./config/mongodb");
+
+const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
+
+mongoose.connect(getMongoDB(), {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
